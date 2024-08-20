23ad1422ebf7abc08e9f48e9a883a0c978face8e
Steven Beazley <steven.beazley@iver.se>
2024-08-20 10:58:00 +0200

    Updated for EU

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-north-1"
 }
 
 provider "random" {}
@@ -21,7 +21,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   user_data     = file("init-script.sh")
 
   tags = {
